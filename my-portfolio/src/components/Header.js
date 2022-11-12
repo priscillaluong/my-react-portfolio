c795d2f9f8493975f1490837a267819de93034ca
P Luong <p-luong@live.co.uk>
2022-11-12 21:38:31 +0000

    added comments in Header.js

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -1,3 +1,20 @@
+/* //object destructuring
+const Header = ({ title }) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+//passing through props from App.js
+const Header = (props) => {
+    return (
+        <header>
+            <h1>{props.title}</h1>
+        </header>
+    )
+} */
+
 const Header = () => {
     return (
         <header>
