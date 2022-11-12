613d1ce197183a573498e12b589d2cc85c82e5bd
P Luong <p-luong@live.co.uk>
2022-11-12 21:33:10 +0000

    created Header component

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello from React</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Priscilla Luong</h1>
+        </header>
+    )
+}
+
+export default Header
