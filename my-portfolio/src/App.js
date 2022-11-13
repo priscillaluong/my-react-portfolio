723628f7166fd55b7ae7ea327a46217a86d2e288
P Luong <p-luong@live.co.uk>
2022-11-13 22:55:39 +0000

    added boiler plate code to contact and portfolio component

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
 
@@ -8,6 +10,13 @@ function App() {
     <div className="container">
       <div className="content">
         <Navbar />
+        <Header />
+      </div>
+      <div className="content">
+        <Portfolio />
+      </div>
+      <div className="content">
+        <Contact />
       </div>
     </div>
   );
