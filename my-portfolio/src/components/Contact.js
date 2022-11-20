8d12b3a046e635ee581a109b0496d398725a93e0
P Luong <p-luong@live.co.uk>
2022-11-20 21:40:58 +0000

    created contact form

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,22 +1,62 @@
+import { useState } from "react";
+
 const Contact = () => {
   const [formSubmit, setFormSubmit] = useState(false);
   const handleSubmit = () => {
     setTimeout(() => {
-      setSubmitted(true);
+        setFormSubmit(true);
     }, 100);
   };
 
-  if (submitted) {
+  if (formSubmit) {
     return (
-        <div className="">
-            <h1>Thank you!</h1>
-        </div>
+      <div className="">
+        <h1>Thank you!</h1>
+      </div>
     );
   }
   return (
-    <div>
-      <h1>This is the contact page</h1>
-    </div>
+    <form
+      action=""
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <div className="mb-3 pt-0">
+        <input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <textarea
+          placeholder="Your message"
+          name="message"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <button
+          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="submit"
+        >
+          Send a message
+        </button>
+      </div>
+    </form>
   );
 };
 
