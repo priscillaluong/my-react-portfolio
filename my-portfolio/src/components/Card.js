7fc6be684896e22057e47cd07e50d9584cd246e8
P Luong <p-luong@live.co.uk>
2022-11-20 22:43:17 +0000

    added project articles

--- my-portfolio/src/components/Card.js
+++ my-portfolio/src/components/Card.js
@@ -1,6 +1,7 @@
 import image1 from '../assets/tradr.png'
 import image2 from '../assets/destination-guide.png'
 import image3 from '../assets/baxters.png'
+import '../styles/Card.css'
 
 const Card = () => {
     return (  
@@ -9,7 +10,23 @@ const Card = () => {
                 <div className="portfolio-image">
                     <img src={image1} alt="tradr marketplace"></img>
                 </div>
-                <h3 className="portfolio-title">This is the portfolio title</h3>
+                <h3 className="portfolio-title">Tradr Marketplace</h3>
+                <a href="/" className="btn">Github Link</a>
+                <a href="/" className="btn">Deployed Application</a>
+            </article>
+            <article className="portfolio-item">
+                <div className="portfolio-image">
+                    <img src={image2} alt="destination guide"></img>
+                </div>
+                <h3 className="portfolio-title">Destination Guide</h3>
+                <a href="/" className="btn">Github Link</a>
+                <a href="/" className="btn">Deployed Application</a>
+            </article>
+            <article className="portfolio-item">
+                <div className="portfolio-image">
+                    <img src={image3} alt="baxters"></img>
+                </div>
+                <h3 className="portfolio-title">Baxters Flowers</h3>
                 <a href="/" className="btn">Github Link</a>
                 <a href="/" className="btn">Deployed Application</a>
             </article>
