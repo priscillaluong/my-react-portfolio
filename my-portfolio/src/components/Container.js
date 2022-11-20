47cd09aacda872eb840578263b750a4d3b314ad3
P Luong <p-luong@live.co.uk>
2022-11-20 19:11:44 +0000

    added useState func to container.js

--- my-portfolio/src/components/Container.js
+++ my-portfolio/src/components/Container.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import Home from './Home';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Experience from './Experience';
+
 const Container = () => {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Experience') {
+            return <Experience />;
+        }
+        return <Contact />;
+    }
     return (
-        <header>
-        </header>
+        <div>
+        </div>
     )
 }
 
-export default Header
+export default Container
