f175496b9ddb19ade3940e1a28c2703a3bb1b768
P Luong <p-luong@live.co.uk>
2022-11-21 16:57:16 +0000

    amended css in homepage

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -6,9 +6,11 @@ const Home = () => {
       <div className="profile-image">
         <img src={profileImage} alt="profile"></img>
       </div>
+      <div className="text-container">
       <h1 className="profile-text">
         A Junior Full Stack Developer with knowledge in the fundamentals of graphics, web development, and a particular interest in new technologies. Highly skilled in digital design and a confident user of various CAD software. 
       </h1>
+      </div>
     </div>
   );
 };
