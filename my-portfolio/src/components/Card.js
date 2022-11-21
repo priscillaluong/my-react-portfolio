d6651a17e6c18c723aebce1d9c72f565754ded6f
P Luong <p-luong@live.co.uk>
2022-11-21 21:40:40 +0000

    added map function to loop through project data

--- my-portfolio/src/components/Card.js
+++ my-portfolio/src/components/Card.js
@@ -1,44 +1,27 @@
-import image1 from '../assets/tradr.png'
-import image2 from '../assets/destination-guide.png'
-import image3 from '../assets/baxters.png'
-import '../styles/Card.css'
+import "../styles/Card.css";
 
-const Card = () => {
-    return (  
-        <div className="portfolio-container">
-            <article className="portfolio-item">
-                <div className="portfolio-image">
-                    <img src={image1} alt="tradr marketplace"></img>
-                </div>
-                <h3 className="portfolio-title">Tradr Marketplace</h3>
-                <p className="project-description">A marketplace application for users to buy, sell and review listed items. App follows the MVC paradigm in its architectural structure, uses Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.</p>
-                <div className="project-links">
-                <a href="/" className="btn">Github Link</a>
-                <a href="/" className="btn">Deployed Application</a>
-                </div>
-            </article>
-            <article className="portfolio-item">
-                <div className="portfolio-image">
-                    <img src={image2} alt="destination guide"></img>
-                </div>
-                <h3 className="portfolio-title">Destination Guide</h3>
-                <div className="project-links">
-                <a href="/" className="btn">Github Link</a>
-                <a href="/" className="btn">Deployed Application</a>
-                </div>
-            </article>
-            <article className="portfolio-item">
-                <div className="portfolio-image">
-                    <img src={image3} alt="baxters"></img>
-                </div>
-                <h3 className="portfolio-title">Baxters Flowers</h3>
-                <div className="project-links">
-                <a href="/" className="btn">Github Link</a>
-                <a href="/" className="btn">Deployed Application</a>
-                </div>
-            </article>
-        </div>
-    );
-}
- 
+const Card = ({ projects }) => {
+  return (
+    <div className="portfolio-container">
+      {projects.map((project) => (
+        <article className="portfolio-item">
+          <div className="portfolio-image">
+            <img src={project.image} alt={project.title}></img>
+          </div>
+          <h3 className="portfolio-title">{project.title}</h3>
+          <p className="project-description">{project.description}</p>
+          <div className="project-links">
+            <a href={project.githubUrl} className="btn">
+              Github Link
+            </a>
+            <a href={project.deployedUrl} className="btn">
+              Deployed Application
+            </a>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
+};
+
 export default Card;
