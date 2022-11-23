8b04bf609c0f32703b97f4736931e0059ecc7c37
P Luong <p-luong@live.co.uk>
2022-11-23 13:43:53 +0000

    amended contact form

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -31,57 +31,45 @@ const Contact = () => {
           return;
       }
 
-      set
+      setName("");
+      setEmail("");
+      setMessage("");
   };
 
-  if (formSubmit) {
-    return (
-      <div className="">
-        <h1>Thank you!</h1>
-      </div>
-    );
-  }
   return (
-    <form
-      action=""
-      onSubmit={handleSubmit}
-      method="POST"
-      target="_blank"
-    >
-      <div className="mb-3 pt-0">
+    <form className="form">
+
         <input
           type="text"
           placeholder="Your name"
           name="name"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className="inputBox"
           required
         />
-      </div>
-      <div className="mb-3 pt-0">
+
+
         <input
           type="email"
           placeholder="Email"
           name="email"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className="inputBox"
           required
         />
-      </div>
-      <div className="mb-3 pt-0">
+      
         <textarea
           placeholder="Your message"
           name="message"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className="inputBoxMessage"
           required
         />
-      </div>
-      <div className="mb-3 pt-0">
+
         <button
-          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-          type="submit"
+          className="btn"
+          type="submit" onClick={handleSubmit}
         >
           Send a message
         </button>
-      </div>
+
     </form>
   );
 };
